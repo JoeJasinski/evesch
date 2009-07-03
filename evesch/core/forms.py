d3a85868de85db646916c09c3317abc344b183c8
jjasinsk <jjasinsk@a9368350-0ea1-4e47-9cc2-732dc10e84a5>
2009-07-03 18:18:26 +0000

    Signup form validation added
    git-svn-id: file:///Users/jjasinsk/Sites/evesch_repo/trunk@207 a9368350-0ea1-4e47-9cc2-732dc10e84a5

--- evesch/core/forms.py
+++ evesch/core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-
+import re
 
 class MessageForm(forms.Form):
     subject = forms.CharField(max_length=50)
@@ -17,18 +17,32 @@ class SignupForm(forms.Form):
     email = forms.EmailField()
     captcha = forms.CharField()
 
+    def clean_username(self):
+        post_username = self.cleaned_data['username']
+        p = re.compile('^\w+$')
+        if not p.match(post_username):
+            raise forms.ValidationError(_("You must use only letters, numbers, or an underscore"))
+        
+        from euser.models import User
+        if User.objects.filter(username=post_username):
+           raise forms.ValidationError(_("User already exists."))  
+        
+        return post_username
+    
+    def clean_captcha(self):
+        post_captcha = self.cleaned_data['captcha']
+        p = re.compile('^[a-zA-Z0-9]+$')
+        if not p.match(post_captcha):
+            raise forms.ValidationError(_("You must use only letters or numbers."))
+        return post_captcha
+
     def clean(self):
         cleaned_data = self.cleaned_data
-        posted_username = cleaned_data.get("username")
         posted_password = cleaned_data.get("password")
         posted_password_verify = cleaned_data.get("password_verify")
     
         if posted_password != posted_password_verify:
             raise forms.ValidationError(_("Passwords to not match."))
-
-        from euser.models import User
-        if User.objects.filter(username=posted_username):
-           raise forms.ValidationError(_("User already exists."))    
         
         return cleaned_data
 
