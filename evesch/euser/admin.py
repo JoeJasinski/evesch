9a498171fe8ed15e25bf39607bbc61257feadefb
Joe Jasinski <joe.jasinski@gmail.com>
2014-03-22 18:27:00 -0500

    fix admin user

--- evesch/euser/admin.py
+++ evesch/euser/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from evesch.euser.models import eUser,UserIM,UserGroup
+from django.contrib.auth.admin import UserAdmin 
 
-class eUserAdmin(admin.ModelAdmin):
-    list_display = ['username','last_name','first_name','gender']
+class eUserAdmin(UserAdmin):
+    pass
 class UserIMAdmin(admin.ModelAdmin):
     list_display = ['im_name','im_protocol_type','im_privacy','im_isDefault']
 class UserGroupAdmin(admin.ModelAdmin):
